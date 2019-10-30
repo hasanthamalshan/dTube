b468e9c12c28af9a1b78bd0824b9bcd8cce9689a
Hasantha malshan <hasanthamalshan07@gmail.com>
2019-10-31 05:05:42 +0530

    created main canvas

---
+++ main.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+from tkinter import filedialog, Text
+import os
+
+root = tk.Tk()
+
+canvas = tk.Canvas(root, height=750, width=500, bg="#041824")
+root.maxsize(500, 750)
+root.minsize(500, 750) 
+canvas.pack()
+
+root.mainloop()
+
+
+
+
