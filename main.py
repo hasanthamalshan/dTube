ed25082300a7b564dc031436d8f74808dc2aa477
Hasantha malshan <hasanthamalshan07@gmail.com>
2019-11-04 23:20:18 +0530

    add download path,video name and progress

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 from tkinter import *
+from tkinter import filedialog
+from tkinter.ttk import Progressbar
 from PIL import ImageTk, Image
 from pytube import YouTube
 from bs4 import BeautifulSoup
@@ -12,23 +14,26 @@ root = tk.Tk()
 root.title("dTube")
 
 videoList = []
+
 var = IntVar()
+videotype = IntVar()
+path = StringVar()
+
+
+def getpath():
+    path1 = filedialog.askdirectory()
+    path.set(path1)
+    urlInput_label = tk.Label(frame,text="Path > " + path.get(),bg="#041824",fg="#ffffff",font=("Courier", 10),wraplength=400)
+    urlInput_label.place(y=440,relwidth=1,relheight=0.05)
 
 def selection(value):
+    videotype.set(value)
     if(value == 1):
         urlInput_label = tk.Label(frame,text="----------Video Details----------",bg="#041824",fg="#ffffff",font=("Courier", 10))
         urlInput_label.place(y=200,relwidth=1,relheight=0.07)
     elif(value == 2):
-        urlInput_label = tk.Label(frame,text="----------PlayList Details----------",bg="#041824",fg="#ffffff",font=("Courier", 10))
+        urlInput_label = tk.Label(frame,text="--------PlayList Details--------",bg="#041824",fg="#ffffff",font=("Courier", 10))
         urlInput_label.place(y=200,relwidth=1,relheight=0.07)
-    
-
-def downloadOne(url):
-    downloadYouTube(url)
-
-def downloadList():
-    for link in videoList:
-        downloadYouTube(link)
 
 def getPlaylistLinks(url):
     html_page = urlopen(url)
@@ -36,20 +41,40 @@ def getPlaylistLinks(url):
     for a in soup.find_all('a'):
             if a.get('href').startswith('/watch'):
                 videoList.append('https://youtube.com' + a.get('href').split('&')[0])
-    print(videoList)
-    print(var)
+    #print(videoList)
 
-def downloadYouTube(videourl,path):
+def downloadYouTube(videourl,downloadpath,name):
+    # progress = Progressbar(frame,orient="horizontal",length=100,mode='determinate')
+    # progress.place(y=450,relwidth=1,relheight=0.01)
     yt = YouTube(videourl)
     yt = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
-    if not os.path.exists(path):
-        os.makedirs(path)
-        yt.download(path)
+    if not os.path.exists(downloadpath):
+        os.makedirs(downloadpath)
+    yt.download(downloadpath)
+   
 
 def enter():
-    getPlaylistLinks(str(urlInput.get()))
-    #downloadYouTube(str(urlInput.get()), "C:/Users/HASANTHA MALSHAN/Desktop")
+    print(videoList)
+    if(videotype.get() == 1):
+        yt1 =  YouTube(urlInput.get())
+        urlInput_label = tk.Label(frame,text=yt1.title,bg="#041824",fg="#ffffff",font=("Courier", 10),wraplength=400)
+        urlInput_label.place(y=260,relwidth=1,relheight=0.2)
+    elif(videotype.get() == 2):
+        getPlaylistLinks(urlInput.get())
+        urlInput_label = tk.Label(frame,text=len(videoList),bg="#041824",fg="#ffffff",font=("Courier", 10),wraplength=400)
+        urlInput_label.place(y=260,relwidth=1,relheight=0.2)
 
+def download():
+    yt1 =  YouTube(urlInput.get())
+    if(videotype.get() == 1):
+        downloadYouTube(str(urlInput.get()),path.get(),yt1.title)
+    elif(videotype.get() == 2):
+        for l in videoList:
+            yt1 =  YouTube(l)
+            get_true = True
+            while get_true:
+                downloadYouTube(l,path.get(),yt1.title)
+                
 
 canvas = tk.Canvas(root, height=760, width=510,bg="#041824")
 root.maxsize(500, 750)
@@ -80,8 +105,11 @@ urlInput.place(y=100,relwidth=1,relheight=0.06)
 urlInput_button = tk.Button(frame, text="Enter",bd=0,padx=20,pady=10, bg='#052978',fg="#ffffff",command=enter)
 urlInput_button.place(x=150,y=155)
 
-urlInput_button = tk.Button(frame, text="Download",bd=0,padx=20,pady=10, bg='#052978',fg="#ffffff")
-urlInput_button.place(x=136,y=500)
+urlInput_button = tk.Button(frame, text="Set Path",bd=0,padx=20,pady=10, bg='#052978',fg="#ffffff",command=getpath)
+urlInput_button.place(x=85,y=500)
+
+urlInput_button = tk.Button(frame, text="Download",bd=0,padx=20,pady=10, bg='#052978',fg="#ffffff",command=download)
+urlInput_button.place(x=200,y=500)
 
 
 root.mainloop()
